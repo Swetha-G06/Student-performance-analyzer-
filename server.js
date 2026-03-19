25c07c6c6b66f55720f6ef5894ae7fa579c8b952
Swetha-G06 <25dtsa61@kristujayanti.com>
2026-03-20 00:10:26 +0530

    initial commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ db.connect(err => {
 // 2. Handle the Data
 app.post('/result', (req, res) => {
     console.log("Received data for:",req.body.name);
-    
+
     const { name, mark1, mark2, mark3 } = req.body;
     
     const total = mark1 + mark2 + mark3;
@@ -51,6 +51,8 @@ app.post('/result', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("🚀 Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server running on port ${PORT}`);
 });
